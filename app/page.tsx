c79f3030409d58acdb2f822a38a710e251d89c10
Juan Diego Serrato <serrato.juand@gmail.com>
2026-04-08 13:24:01 -0600

    fixed format bug for small screens

--- app/page.tsx
+++ app/page.tsx
@@ -251,7 +251,7 @@ export default async function LandingPage() {
             fontFamily: "system-ui, sans-serif",
             fontWeight: "500",
           }}>
-            Create your account
+            Get started
           </Link>
         </div>
       </section>
@@ -287,28 +287,3 @@ export default async function LandingPage() {
 
 
 
-
-
-
-
-
-// WHAT THIS FILE DOES:
-// This is the root page at localhost:3000.
-// It immediately redirects logged in users to the dashboard
-// and logged out users to the login page.
-// Nobody should ever actually see this page.
-
-/*
-import { getServerSession } from "next-auth"
-import { redirect } from "next/navigation"
-
-export default async function RootPage() {
-  const session = await getServerSession()
-
-  if (session) {
-    redirect("/dashboard")
-  } else {
-    redirect("/login")
-  }
-}
-  */
